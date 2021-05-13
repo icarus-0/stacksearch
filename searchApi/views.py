8b2b70139e643c2cf1df3fad6894fd029cf12751
icarus-0 <46519285+icarus-0@users.noreply.github.com>
2021-05-13 22:48:51 +0530

    Update views.py

--- searchApi/views.py
+++ searchApi/views.py
@@ -13,52 +13,17 @@ from datetime import date
 import pytz
 # Create your views here.
 
+search_timestamp = [0]*5
+
+try:
+    with open('./data/cached.json') as json_file:
+        cached_data = json.load(json_file)
+except:
+    cached_data = {}
+
 
 def home(request):
-    return render(request, 'index_login.html')
-
-
-@csrf_exempt
-def handleSignup(request):
-    if request.method == 'POST':
-        username = request.POST['signup_username']
-        email = request.POST['signup_email']
-        pass1 = request.POST['signup_pass1']
-        pass2 = request.POST['signup_pass2']
-        if pass1 != pass2:
-            return redirect('/signup')
-
-        try:
-            new_user = User.objects.create_user(username, email, pass1)
-        except:
-            return redirect('/signup')
-        new_user.save()
-        print('User created')
-        return redirect('/signup')
-    else:
-        return render(request, 'index_login.html')
-
-
-@csrf_exempt
-def handlelogin(request):
-    if request.method == "POST":
-        username = request.POST['signin_username']
-        password = request.POST['signin_password']
-
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('/stacksearch')
-        else:
-            return redirect('/signup')
-
-
-@csrf_exempt
-def handlelogout(request):
-    print(request.user)
-    logout(request)
-    return redirect('/')
+    return render(request, 'stackSearch.html')
 
 
 def stacksearch(request):
@@ -87,72 +52,86 @@ def searchData(request):
     query = request.POST['query']
 
     api_url = 'https://api.stackexchange.com/2.2/questions?'
-
+    cacheid = ''
     if page != '':
         api_url += 'page='+page+'&'
+        cacheid += page
 
     if pagesize != '':
         api_url += 'pagesize='+pagesize+'&'
+        cacheid += pagesize
 
     if fromdate != '':
         fromdate = getTimeStamp(fromdate)
         api_url += 'fromdate='+fromdate+'&'
+        cacheid += fromdate
 
     if todate != '':
         todate = getTimeStamp(todate)
         api_url += 'fromdate='+todate+'&'
+        cacheid += todate
 
     if order != 'Choose...':
         api_url += 'order='+order+'&'
+        cacheid += order
 
     if min != '':
         min = getTimeStamp(min)
         api_url += 'order='+min+'&'
+        cacheid += min
 
     if max != '':
         max = getTimeStamp(max)
         api_url += 'order='+max+'&'
+        cacheid += max
 
     if sort != 'Choose...':
         api_url += 'sort='+sort+'&'
+        cacheid += sort
 
     if tag != '':
         api_url += 'tagged='+tag+'&'
+        cacheid += tag
 
     api_url += 'site=stackoverflow'
+    cacheid += query
 
-    valid, res = increase_search_count(user, query, api_url)
+    valid, res = increase_search_count(query, api_url, cacheid)
     if not valid:
         return HttpResponse('<h1>Limit Exceeded</h1>')
-    html = get_search_html(res['items'])
+    html = get_search_html(res['items'], query)
     return HttpResponse(html)
 
 
-def get_search_html(data):
+def get_search_html(data, query):
     html = ""
-
+    checker = False
     for i in data:
-        html += ''' <div class="card w-50 mx-auto mt-3">
-        <div class="card-header">
-          <a href="">
-            <img
-              src =" '''+i['owner']['profile_image'] + '''"
-              alt="https://visualpharm.com/assets/30/User-595b40b85ba036ed117da56f.svg"
-              style="width: 50px; height: 50px"
-            />
-            <h6>'''+i['owner']['display_name'] + '''</h6>
-          </a>
-        </div>
-        <div class="card-body">
-          <blockquote class="blockquote mb-0">
-          <a href="'''+i['link'] + '''">
-            <p>'''+i['title']+'''</p>
-          </a>
-            <footer class="blockquote-footer"> Tags ''' + str(i['tags']) + '''</footer>
-          </blockquote>
-        </div>
-      </div>'''
+        if query in i['title'] or i['title'] in query:
+            checker = True
+            html += ''' <div class="card w-50 mx-auto mt-3">
+                    <div class="card-header">
+                    <a href="">
+                        <img
+                        src =" '''+i['owner']['profile_image'] + '''"
+                        alt="https://visualpharm.com/assets/30/User-595b40b85ba036ed117da56f.svg"
+                        style="width: 50px; height: 50px"
+                        />
+                        <h6>'''+i['owner']['display_name'] + '''</h6>
+                    </a>
+                    </div>
+                    <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                    <a href="'''+i['link'] + '''">
+                        <p>'''+i['title']+'''</p>
+                    </a>
+                        <footer class="blockquote-footer"> Tags ''' + str(i['tags']) + '''</footer>
+                    </blockquote>
+                    </div>
+                </div>'''
 
+    if not checker:
+        return '''<h1>Sorry We Didn't find any matching Question title with your query ..!</h1>'''
     return html
 
 
@@ -172,35 +151,31 @@ def get_total_time(p, p1):
     return minute
 
 
-def increase_search_count(user, searched, apiurl):
-    d = searchedData.objects.filter(
-        user=user, date=str(date.today())).order_by('id')
+def increase_search_count(searched, apiurl, cacheid):
+    d = cached_data
     tz = pytz.timezone('Asia/Kolkata')
 
-    for i in d:
-        if searched.strip() == str(i.search).strip():
-            return True, i.response
+    for i in cached_data:
+        if cacheid.strip() == i.strip():
+            return True, cached_data[i][0]
 
-    if len(d) > 5:
-        d = list(d)
+    if len(d.keys()) > 5:
+        d = list(d.keys())
         ins = d[-4]
         now = datetime.datetime.now(tz)
-        usagetime = get_total_time(ins.time, now)
+
+        usagetime = get_total_time(cached_data[ins][1], now)
 
         if usagetime < 1:
             return False, ''
 
-    elif len(d) > 100:
+    elif len(d.keys()) > 100:
         return False, ''
 
     response = get_response(apiurl)
 
-    ins = searchedData(user=user,
-                       date=str(date.today()),
-                       search=searched,
-                       response=response,
-                       time=str(datetime.datetime.now(tz)),
-                       )
-    ins.save()
+    cached_data[cacheid] = [response, str(datetime.datetime.now(tz))]
+    with open("./data/cached.json", "w") as fp:
+        json.dump(cached_data, fp)
 
     return True, response
