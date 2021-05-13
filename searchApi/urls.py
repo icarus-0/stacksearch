2165827dbf7961b793b974c68bef82ee5e250608
icarus-0 <46519285+icarus-0@users.noreply.github.com>
2021-05-13 22:49:16 +0530

    Update urls.py

--- searchApi/urls.py
+++ searchApi/urls.py
@@ -5,9 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup', views.handleSignup, name="handleSignup"),
-    path('login', views.handlelogin, name="handlelogin"),
-    path('logout', views.handlelogout, name="handlelogout"),
+
     path('stacksearch', views.stacksearch, name="stacksearch"),
     path('searchData', views.searchData, name="searchData")
 ]
